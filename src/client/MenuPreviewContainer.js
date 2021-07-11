eb81f8df69ab29806a0e88ba012c05b5fd2c8102
Bete Yemane <beteyemane@hotmail.co.uk>
2021-07-11 11:52:29 +0100

    Menu preview card component

--- src/client/MenuPreviewContainer.js
+++ src/client/MenuPreviewContainer.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import MenuPreviewCard from "./MenuPreviewCard"
+import MenuPreviewCard from "./MenuPreviewCard";
 
-const MenuPreviewContainer = ({selectedItems, setSelectedItems}) => {
+const MenuPreviewContainer = ({ selectedItems, setSelectedItems }) => {
+  const removeItem = (e, item) => {
+    e.preventDefault();
+    const newSelectedItemsList = selectedItems.filter(
+      (selectedItem) => selectedItem != item
+    );
+    setSelectedItems(newSelectedItemsList);
+  };
 
-    const removeItem = (e, item) => {
-        e.preventDefault();
-        const newSelectedItemsList = selectedItems.filter((selectedItem) => selectedItem != item);
-        setSelectedItems(newSelectedItemsList);
-    
-        // const newSelectedDietariesList = selectedDietaries.filter((selectedItem) => selectedItem.dietaries != item.dietaries);
-        // setSelectedDietaries(newSelectedDietariesList);
-        // groupDietaries()
-    }
+  return (
+    <div className="col-7">
+      <h2>Menu preview</h2>
+      {selectedItems.map((selectedItem) => (
+        <MenuPreviewCard selectedItem={selectedItem} removeItem={removeItem} />
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div className="col-7">
-            <h2>Menu preview</h2>        
-            {selectedItems.map(selectedItem => 
-                <MenuPreviewCard selectedItem={selectedItem} removeItem={removeItem}/>
-            )}
-        </div>
-    )
-}
-
-export default MenuPreviewContainer
+export default MenuPreviewContainer;
