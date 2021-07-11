f3ab85e0e780e17406ce1292eb5a5f53ff303a50
Bete Yemane <beteyemane@hotmail.co.uk>
2021-07-11 11:52:56 +0100

    Tidy up on MenuItemsContainer

--- src/client/MenuItemsContainer.js
+++ src/client/MenuItemsContainer.js
@@ -1,51 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import MenuItemCard from './MenuItemCard'
-import SearchBar from './SearchBar';
+import MenuItemCard from "./MenuItemCard";
 
-const MenuItemsContainer = ({items, selectedItems, setSelectedItems}) => {
-
-  const [searchTerm, setSearchTerm] = useState('')
-
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value)
-  }
-
-  const selectItem = (e ,item) => {
+const MenuItemsContainer = ({ items, selectedItems, setSelectedItems }) => {
+  const selectItem = (e, item) => {
     e.preventDefault();
-    if(!selectedItems.includes(item)){
-      setSelectedItems(selectedItems.concat(item))
-      
-      // const groupedArr = []
-      // //item.dietaries.map(i => groupedArr.push(i))
-      // groupedArr.concat(item.dietaries)
-      // console.log("grouped arr", groupedArr)
-      // groupedArr.reduce((acc, curr) => {
-      //   if (!groupedDietaries.hasOwnProperty(curr)) {
-      //     acc[curr] = 1;
-      //   } else {
-      //     acc[curr] += 1;
-      //   }
-      //   return acc;
-      // }, {}); 
-      // setGroupedDietaries(groupedArr)    
-    } 
+    if (!selectedItems.includes(item)) {
+      setSelectedItems(selectedItems.concat(item));
+    }
   };
 
   return (
-      <div className="col-5"> 
-         <SearchBar handleChange={handleChange} />
-         {items.filter((val) => {
-            if(val == "") {
-              return val
-             } else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-             return val
-            }
-          }).map(item => 
-            <MenuItemCard item={item} selectItem={selectItem}/>
-        )}
-      </div>
-  )
-}
+    <div className="col-5">
+      {items.map((item) => (
+        <MenuItemCard item={item} selectItem={selectItem} />
+      ))}
+    </div>
+  );
+};
 
-export default MenuItemsContainer
+export default MenuItemsContainer;
