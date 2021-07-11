d680af5ef410aae942ae06ec8e27a2a41c1b722b
Bete Yemane <beteyemane@hotmail.co.uk>
2021-07-11 12:33:28 +0100

    Dietary summary method.

---
+++ src/client/components/Header/DietaryList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const groupDietaries = (items) => {
+  //grouping the selected dietaries
+  //make the dietary the key and add 1 to value everytime it gets added to the selected list
+
+  return items.reduce((acc, curr) => {
+    curr.dietaries.forEach((dietary) => {
+      if (!acc.hasOwnProperty(dietary)) {
+        acc[dietary] = 1;
+      } else {
+        acc[dietary] += 1;
+      }
+    });
+    return acc;
+  }, {});
+};
+
+const DietaryList = ({ selectedItems }) => {
+  return (
+    <div className="col-6 menu-summary-right">
+      {Object.entries(groupDietaries(selectedItems)).map(([key, value]) => (
+        <span key={key}>
+          {value}x <span className="dietary">{key}</span>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default DietaryList;
